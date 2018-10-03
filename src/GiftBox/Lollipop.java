079fa4f379562b2f0582c2bd8cda11d5fba1d52e
CHERNYSHEV Artem <artem.chernyshev@raiffeisen.ru>
2018-10-03 20:33:24 +0300

    Collection

--- src/GiftBox/Lollipop.java
+++ src/GiftBox/Lollipop.java
@@ -6,4 +6,5 @@ public class Lollipop extends Sweet {
     public Lollipop(String name, Integer count, double price, Double weight) {
         super(name, count, price, weight);
     }
+    c
 }
--- src/GiftBox/Main.java
+++ src/GiftBox/Main.java
@@ -27,27 +27,27 @@ public class Main {
 
 
 
-
-        System.out.println("Вторая сладость:");
-        String caramelNeme = scanner.next();
-        System.out.println("Количество:");
-        Integer countCaramel = scanner.nextInt();
-        System.out.println( "Укажите цену:" );
-        Double caremelPrice = scanner.nextDouble();
-        System.out.println( "Укажаите вес:" );
-        Double caremelWeight = scanner.nextDouble();
-
-
-
-
-        System.out.println("Третья сладость:");
-        String marmeladeName = scanner.next();
-        System.out.println("Количество:");
-        Integer countMarmelade = scanner.nextInt();
-        System.out.println( "Укажите цену:" );
-        Double marmeladePrice = scanner.nextDouble();
-        System.out.println( "Укажаите вес:" );
-        Double marmeladeWeight = scanner.nextDouble();
+//
+//        System.out.println("Вторая сладость:");
+//        String caramelNeme = scanner.next();
+//        System.out.println("Количество:");
+//        Integer countCaramel = scanner.nextInt();
+//        System.out.println( "Укажите цену:" );
+//        Double caremelPrice = scanner.nextDouble();
+//        System.out.println( "Укажаите вес:" );
+//        Double caremelWeight = scanner.nextDouble();
+//
+//
+//
+//
+//        System.out.println("Третья сладость:");
+//        String marmeladeName = scanner.next();
+//        System.out.println("Количество:");
+//        Integer countMarmelade = scanner.nextInt();
+//        System.out.println( "Укажите цену:" );
+//        Double marmeladePrice = scanner.nextDouble();
+//        System.out.println( "Укажаите вес:" );
+//        Double marmeladeWeight = scanner.nextDouble();
 
 
 //        Map <String, Integer> sweets = new TreeMap<>();
@@ -59,37 +59,38 @@ public class Main {
 
 
 
-        Sweet[] present = {new Lollipop(lollipopName, countLollipop, lollipopPrice * countLollipop, lollipopWeight * countLollipop),
-                new Caramel(caramelNeme, countCaramel, caremelPrice * countCaramel, caremelWeight * countCaramel),
-                new Marmalade(marmeladeName, countMarmelade,marmeladePrice * countMarmelade, marmeladeWeight * countMarmelade)
-        };
-
+        List present = new ArrayList();
+        present.addAll
+                ( (Collection) new Lollipop(lollipopName, countLollipop, lollipopPrice * countLollipop, lollipopWeight * countLollipop));
+//                        (Collection) new Caramel(caramelNeme, countCaramel, caremelPrice * countCaramel, caremelWeight * countCaramel),
+//                        (Collection) new Marmalade(marmeladeName, countMarmelade,marmeladePrice * countMarmelade, marmeladeWeight * countMarmelade)
 
 //        Caramel Caramel1 = new Caramel( "АО", 1, 2, 3 );
+        System.out.println (present.get ( 0 ));
 
 
 
 
-
-
-        double totalWeight = 0;
-        double totalPrice = 0;
-
-
-        System.out.println("НОВОГОДНИЙ ПОДАРОК:");
-
-        for (int i = 0; i < present.length; i++ ){
-
-            System.out.println(present[i].toString());
-
-            totalWeight = totalWeight+present[i].getWeight();
-            totalPrice = totalPrice+present[i].getPrice();
-
-        }
-
-        System.out.println("Общий вес: " + totalWeight);
-        System.out.println("Общая цена " + totalPrice);
-
+//
+//
+//        double totalWeight = 0;
+//        double totalPrice = 0;
+//
+//
+//        System.out.println("НОВОГОДНИЙ ПОДАРОК:");
+//
+//        for (int i = 0; i < present.get(0); i++ ){
+//
+//            System.out.println ( present.get(0).toString() );
+////
+////            totalWeight = totalWeight+present.get().getWeight();
+////            totalPrice = totalPrice+present.get().getPrice();
+//
+//        }
+//
+//        System.out.println("Общий вес: " + totalWeight);
+//        System.out.println("Общая цена " + totalPrice);
+//
 
 
     }
