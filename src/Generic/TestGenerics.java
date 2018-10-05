d7d293b7fda0e1a5188665e145f249c6e0e29148
CHERNYSHEV Artem <artem.chernyshev@raiffeisen.ru>
2018-10-05 18:09:37 +0300

    update class TestGenerics

--- src/Generic/TestGenerics.java
+++ src/Generic/TestGenerics.java
@@ -1,36 +1,38 @@
 package Generic;
 
-public class TestGenerics{
+public class TestGenerics <T1, T2, T3>{
 
-    String testStr;
-    int testInt;
-    Double testDoub;
+    private T1 obgect1;
+    private T2 obgect2;
+    private T3 obgect3;
 
-
-    public String getTestStr(){
-        return testStr;
+    TestGenerics(T1 name, T2 count, T3 price){
+        this.obgect1=name;
+        this.obgect2=count;
+        this.obgect3=price;
     }
 
-    public int getTestInt(){
-        return testInt;
+    public T1 getName(){
+        return obgect1;
     }
 
-    public Double getTestDoub(){
-        return testDoub;
+    public T2 getCount(){
+        return obgect2;
     }
 
-    public void setTestStr(String testStr){
-        this.testStr=testStr;
-    }
-
-    public void setTestInt(int testInt){
-        this.testInt=testInt;
-    }
-
-    public void setTestDoub(Double testDoub){
-        this.testDoub=testDoub;
-    }
-
-    public TestGenerics(){
+    public T3 getPrice(){
+        return obgect3;
     }
 }
+
+    class PrintArray{
+        public static void main(String[] args){
+            TestGenerics <String, Integer, Double> printArray = new TestGenerics <>("Milk", 2, 55.02);
+
+            System.out.println(
+                    printArray.getName() + " Count: " +
+                            printArray.getCount() + " Price: " +
+                            printArray.getPrice() * printArray.getCount());
+        }
+    }
+
