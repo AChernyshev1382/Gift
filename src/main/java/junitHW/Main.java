e3f75567cf4837b3efc272252cbdbcd4b54c4d31
CHERNYSHEV Artem <artem.chernyshev@raiffeisen.ru>
2018-10-25 12:54:55 +0300

    add package junitHW

---
+++ src/main/java/junitHW/Main.java
@@ -0,0 +1,10 @@
+//package junitHW;
+//
+//public class Main{
+//    public static void main(String[] args){
+//        CalculationAreaCircle calculationAreaCircle = new CalculationAreaCircle( 2 );
+//
+//        System.out.println(calculationAreaCircle.getAreaCircle());
+//        System.out.println(Math.PI);
+//    }
+//}
